8ddefc63bb9ddcc4e4643151caa7d956f40b5bef
Clara Aymerich Moreno <clara.aymerich@estudiantat.upc.edu>
2022-03-08 14:02:50 +0100

    codigo modificado en clase

---
+++ src/pav_analysis.c
@@ -0,0 +1,30 @@
+#include <math.h>
+#include "pav_analysis.h"
+
+float compute_power(const float *x, unsigned int N) {
+    float sum=1e-12;
+    for(unsigned int i=0;i<N;i++){
+
+        sum=sum+x[i]*x[i];
+    }
+    return 10*log10(sum/N);
+}
+
+float compute_am(const float *x, unsigned int N) {
+    float sum=0;
+    for(unsigned int i=0;i<N;i++){
+        sum=sum+fabs(x[i]);
+    }
+    return sum/N;
+}
+
+float compute_zcr(const float *x, unsigned int N, float fm) {
+    float zcr,sum=0;
+    int i;
+    for( i=0;i<N;i++){
+        if(x[i]*x[i-1]<0){
+            sum=sum+1;
+        }
+    }
+    return fm*sum/(2*(N-1));
+}
--- src/vad.c
+++ src/vad.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "pav_analysis.h"
 #include "vad.h"
 
 const float FRAME_TIME = 10.0F; /* in ms. */
@@ -42,7 +43,10 @@ Features compute_features(const float *x, int N) {
    * For the moment, compute random value between 0 and 1 
    */
   Features feat;
-  feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  //feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+  feat.zcr = compute_zcr(x,N,16000);
+  feat.am=compute_am(x,N);
+  feat.p=compute_pow(x,N);
   return feat;
 }
 
@@ -82,6 +86,7 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
   /* 
    * TODO: You can change this, using your own features,
    * program finite state automaton, define conditions, etc.
+   * tiene dos argumentos : vad_data --> nos informa del estado y c --> señal
    */
 
   Features f = compute_features(x, vad_data->frame_length);
@@ -90,15 +95,17 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
   switch (vad_data->state) {
   case ST_INIT:
     vad_data->state = ST_SILENCE;
+    vad_data->p1 = f.p + 10;
+    //nuestro umbral es el 10 (random)
     break;
 
   case ST_SILENCE:
-    if (f.p > 0.95)
+    if (f.p > vad_data->p1)
       vad_data->state = ST_VOICE;
     break;
 
   case ST_VOICE:
-    if (f.p < 0.01)
+    if (f.p < vad_data->p1)
       vad_data->state = ST_SILENCE;
     break;
 
